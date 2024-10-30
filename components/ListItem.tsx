ea575218823abf35c7a0867fc30933d0401c32c3
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-30 14:49:44 +0100

    Add ListItem and SwipeDeleteAnimation components

---
+++ components/ListItem.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ListItem = () => {
+  return (
+    <View>
+      <Text>ListItem</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ListItem;
---
+++ components/SwipeDeleteAnimation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const SwipeDeleteAnimation = () => {
+  return (
+    <View>
+      <Text>SwipeDeleteAnimation</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SwipeDeleteAnimation;
